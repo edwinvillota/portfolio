6918e81a501576ba85abd34cbeb0e3357493ed6d
edwinvillota <edwinvillota@hotmail.com>
2022-07-23 20:50:51 -0500

    Update about me and fixed fonts

--- src/containers/AboutMe/index.jsx
+++ src/containers/AboutMe/index.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import { Section, Title, Grid, Paragraph, StyledAboutIlustration } from './styles'
-import { Fade } from '../../components/Fade'
+import React from "react";
+import {
+  Section,
+  Title,
+  Grid,
+  Paragraph,
+  StyledAboutIlustration,
+} from "./styles";
+import { Fade } from "../../components/Fade";
 export const AboutMe = () => {
   return (
     <Section>
-      <Fade direction='up'>
+      <Fade direction="up">
         <Title>About Me</Title>
       </Fade>
       <Grid>
         <Fade>
           <Paragraph>
             I&apos; a passionate about technology and software development.
-            I&apos; always looking for learn new skills. Working in software
-            is my dream and live doing what i love, sounds perfect to me!.
+            I&apos; always looking for learn new skills and technical
+            challenges.
           </Paragraph>
         </Fade>
-        <StyledAboutIlustration/>
+        <StyledAboutIlustration />
         <Fade>
           <Paragraph>
-            On my free time, I like play video games, learn something new,
-            listen music and wach videos on Youtube.
+            I like play video games, watch series or movies and listen to music.
           </Paragraph>
         </Fade>
       </Grid>
     </Section>
-  )
-}
+  );
+};
--- src/containers/Hero/index.jsx
+++ src/containers/Hero/index.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import { HeroSection, Grid, Title, Description, StyledHeroIlustration } from './styles'
-import { NavBar } from '../../components/NavBar'
+import React from "react";
+import {
+  HeroSection,
+  Grid,
+  Title,
+  Description,
+  StyledHeroIlustration,
+} from "./styles";
+import { NavBar } from "../../components/NavBar";
 
 export const Hero = () => (
   <HeroSection>
     <NavBar />
     <Grid>
       <Title>
-        Hello, My name is Edwin Villota and I&apos;m a Javascript Developer
+        Hello, My name is Edwin Villota and I&apos;m Javascript Developer
       </Title>
       <StyledHeroIlustration />
       <Description>
-        I am from Colombia, I am 26 years old and 2 years of experience with web
-        technologies. I love programming and learn something new every day.
+        I am from Colombia, I am 28 years old and 5 years of experience
+        programming with Javascript. I love software development and learn
+        something new every day.
       </Description>
     </Grid>
   </HeroSection>
-)
+);
